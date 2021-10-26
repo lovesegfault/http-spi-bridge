11d73569dad75c882e1719a35ee398f3377e94c5
Bernardo Meurer <bernardo@meurer.org>
2021-10-26 10:55:27 -0700

    refactor(flake): correctly use crossSystem

--- flake.nix
+++ flake.nix
@@ -1,16 +1,15 @@
 {
   inputs = {
     fenix = {
-      url = "github:figsoda/fenix";
+      url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.naersk.follows = "naersk";
     };
     flake-utils.url = "github:numtide/flake-utils";
-    naersk = {
-      url = "github:nmattia/naersk";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,84 +17,68 @@
     };
   };
 
-  outputs = { self, fenix, flake-utils, naersk, nixpkgs, pre-commit-hooks }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, fenix, flake-utils, gitignore, nixpkgs, pre-commit-hooks }:
+    flake-utils.lib.eachDefaultSystem (localSystem:
       let
-        pkgs =
-          if "${system}" == "aarch64-linux" then
-            nixpkgs.legacyPackages.${system}
-          else
-            nixpkgs.legacyPackages.${system}.pkgsCross.aarch64-multiplatform-musl;
+        pkgs = import nixpkgs {
+          inherit localSystem;
+          crossSystem = nixpkgs.lib.systems.examples.aarch64-multiplatform-musl;
+          overlays = [ fenix.overlay gitignore.overlay ];
+          crossOverlays = [
+            (final: _: {
+              rustToolchain = with final.pkgsBuildHost.fenix; combine [
+                stable.rustc
+                stable.cargo
+                targets.${final.stdenv.targetPlatform.config}.stable.rust-std
+              ];
 
-        fenixPkgs = fenix.packages.${system};
-
-        target = "aarch64-unknown-linux-musl";
-
-        rustFull = with fenixPkgs; combine [
-          (latest.withComponents [
-            "cargo"
-            "clippy-preview"
-            "rust-src"
-            "rust-std"
-            "rustc"
-            "rustfmt-preview"
-          ])
-          targets.${target}.latest.rust-std
-        ];
-
-        naerskBuild = (naersk.lib.${system}.override {
-          stdenv = pkgs.llvmPackages_11.stdenv;
-          cargo = rustFull;
-          rustc = rustFull;
-        }).buildPackage;
-
-        cargoConfig = {
-          CARGO_BUILD_TARGET = target;
-          RUSTFLAGS = "-C linker-flavor=ld.lld -C target-feature=+crt-static";
+              rustPlatform = final.makeRustPlatform {
+                cargo = final.rustToolchain;
+                rustc = final.rustToolchain;
+              };
+            })
+          ];
         };
       in
       {
-        packages.http-spi-bridge = naerskBuild ({
-          src = ./.;
-
-          nativeBuildInputs = with pkgs.pkgsBuildBuild.llvmPackages_11; [ clang lld ];
-
-          dontPatchELF = true;
-        } // cargoConfig);
-
-        defaultPackage = self.packages.${system}.http-spi-bridge;
-
-        devShell = pkgs.mkShell ({
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        packages.http-spi-bridge = pkgs.rustPlatform.buildRustPackage {
           name = "http-spi-bridge";
 
-          nativeBuildInputs = (self.defaultPackage.${system}.nativeBuildInputs or [ ])
-            ++ (with pkgs.pkgsBuildBuild; [
+          src = pkgs.gitignoreSource ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+
+          RUSTFLAGS = "-C target-feature=+crt-static";
+        };
+
+        defaultPackage = self.packages.${localSystem}.http-spi-bridge;
+
+        devShell = pkgs.mkShell {
+          name = "http-spi-bridge";
+
+          inputsFrom = [ self.defaultPackage.${localSystem} ];
+
+          nativeBuildInputs = with pkgs.pkgsBuildBuild; [
             cargo-audit
             cargo-bloat
             cargo-edit
             cargo-udeps
-            fenixPkgs.rust-analyzer
+            rust-analyzer-nightly
             file
             nix-linter
             nixpkgs-fmt
             qemu
-          ]);
+          ];
 
-          buildInputs = (self.defaultPackage.${system}.buildInputs or [ ]);
-        } // cargoConfig);
+          inherit (self.defaultPackage.${localSystem}) RUSTFLAGS;
+          inherit (self.checks.${localSystem}.pre-commit-check) shellHook;
+        };
 
-        checks.pre-commit-check = (pre-commit-hooks.lib.${system}.run {
+        checks.pre-commit-check = (pre-commit-hooks.lib.${localSystem}.run {
           src = ./.;
           hooks = {
             nix-linter.enable = true;
             nixpkgs-fmt.enable = true;
-            rustfmt = {
-              enable = true;
-              entry = pkgs.lib.mkForce ''
-                bash -c 'PATH="$PATH:${rustFull}/bin" cargo fmt -- --check --color always'
-              '';
-            };
           };
         });
       });
